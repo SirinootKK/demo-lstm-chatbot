8c632efefa03950608ee01f197d328a1862ea5c8
SirinootKK <sirinoot981@gmail.com>
2023-08-21 03:05:11 +0700

    fix:user input bar

--- src/components/ChatBox.jsx
+++ src/components/ChatBox.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from "react";
-import { Input, Button } from "@material-tailwind/react";
+import { Input } from "@material-tailwind/react";
 import { userImg, botImg } from "./images/index.js";
 import SendIcon from "@mui/icons-material/Send";
 
@@ -21,6 +21,7 @@ function ChatBox() {
     e.preventDefault();
     if (!userMessage) return;
 
+    setUserMessage("");
     setIsLoading(true);
 
     setBotResponses((prevResponses) => [
@@ -42,26 +43,25 @@ function ChatBox() {
         ...prevResponses,
         { message: data.response, isUserMessage: false },
       ]);
-
-      setUserMessage("");
     } catch (error) {
       console.error("Error fetching response:", error);
+    } finally {
+      setIsLoading(false);
     }
-    setIsLoading(false);
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-primary">
-      <div className="w-full max-w-5xl h-full md:h-5/6 bg-secondary rounded-xl shadow-xl flex flex-col">
+    <div className="flex justify-center items-center h-screen w-screen bg-primary">
+      <div className="w-full max-w-5xl h-full md:m-4 md:h-5/6 bg-secondary md:rounded-xl shadow-xl flex flex-col">
         <h1 className="text-center font-semibold text-secondaryLight mb-4 mt-5 text-lg h-16 flex items-center justify-center">
-          ChatLSTM
+          {isLoading ? "Loading..." : "ChatLSTM"}
         </h1>
         <div className="h-[2px] bg-primary border-0 w-full shadow-xl" />
         <div className="flex-grow overflow-auto h-full" ref={chatContainerRef}>
           {botResponses.map((response, index) => (
             <div
               key={index}
-              className={`flex flex-row items-center space-x-2 px-[48px] py-6 text-white ${
+              className={`flex flex-row md:items-center space-x-2 px-[40px] md:px-[72px] py-6 text-white ${
                 response.isUserMessage ? "bg-secondary" : "bg-purple "
               }`}
             >
@@ -74,7 +74,7 @@ function ChatBox() {
             </div>
           ))}
         </div>
-        <div className="h-28 w-full flex items-center">
+        <div className="h-36 w-full flex items-center">
           <form
             onSubmit={handleSubmit}
             className="flex w-full h-fit relative px-12"
@@ -84,15 +84,16 @@ function ChatBox() {
               value={userMessage}
               className="w-full flex rounded-xl p-4 bg-primaryLight shadow-2xl text-white outline-none"
               onChange={(e) => setUserMessage(e.target.value)}
-              placeholder="Send a message..."
+              placeholder={isLoading ? "Loading..." : "Send a message..."}
+              disabled={isLoading}
             />
-            <Button
+            <button
               type="submit"
               className="z-20 w-fit h-fit text-secondaryLight absolute right-16 top-1/2 -translate-y-1/2"
               disabled={isLoading}
             >
-              {isLoading ? "Loading..." : <SendIcon fontSize="small" />}
-            </Button>
+              {!isLoading ? <SendIcon fontSize="small" /> : null}
+            </button>
           </form>
         </div>
       </div>
